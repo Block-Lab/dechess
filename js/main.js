475e92ddf9d4a14cbc93088aa68af7736a71d0cb
mukundan314 <mukundan314@github.com>
2018-08-15 11:33:16 +0530

    Allow only legal moves

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-var cg = Chessground(document.getElementById('dirty'))
+function toDests(chess) {
+  const dests = {};
+  chess.SQUARES.forEach(s => {
+    const ms = chess.moves({square: s, verbose: true});
+    if (ms.length) dests[s] = ms.map(m => m.to);
+  });
+  return dests;
+}
+
+function toColor(chess) {
+  return (chess.turn() === 'w') ? 'white' : 'black';
+}
+
+function playOtherSide(cg, chess) {
+  return (orig, dest) => {
+    chess.move({from: orig, to: dest});
+    cg.set({
+      turnColor: toColor(chess),
+      movable: {
+        color: toColor(chess),
+        dests: toDests(chess)
+      }
+    });
+  };
+}
+
+const chess = new Chess();
+var options = {
+      movable: {
+        color: 'white',
+        free: false,
+        dests: toDests(chess)
+      }
+}
+
+ground = Chessground(document.getElementById("dirty"), options);
+
+ground.set({
+  movable: { events: { after: playOtherSide(ground, chess) } }
+});

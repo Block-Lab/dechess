87f3030fb57e140fee8e2ce30a59c2a6b759ba73
mukundan314 <mukundan314@github.com>
2018-08-18 18:00:56 +0530

    Add a way to choose themes

--- js/main.js
+++ js/main.js
@@ -25,6 +25,30 @@ function playOtherSide(cg, chess) {
   };
 }
 
+function setBackground(element, background) {
+  element.classList.remove('blue', 'brown', 'green', 'purple')
+  element.classList.add(background)
+}
+
+function setPieceSet(element, pieceSet) {
+  element.classList.remove(
+    'alpha', 'cburnett', 'chess7', 'chessnut',
+    'companion', 'fantasy', 'letter', 'merida',
+    'pirouetti', 'reillycraig', 'riohacha', 'shapes',
+    'spatial')
+  element.classList.add(pieceSet)
+}
+
+function backgroundSelect() {
+  var background = document.getElementById('background').value
+  setBackground(document.getElementById('board'), background)
+}
+
+function pieceSetSelect() {
+  var pieceSet = document.getElementById('pieceSet').value
+  setPieceSet(document.getElementById('board'), pieceSet)
+}
+
 const chess = new Chess();
 var options = {
       movable: {
@@ -39,3 +63,6 @@ ground = Chessground(document.getElementById("board"), options);
 ground.set({
   movable: { events: { after: playOtherSide(ground, chess) } }
 });
+
+backgroundSelect()
+pieceSetSelect()
